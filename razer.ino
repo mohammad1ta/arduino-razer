de561a9c35f49f9b1f8190215c041e4855bae213
Mohammadreza Yektamaram <mr.yekta@yahoo.com>
2019-10-12 11:10:31 +0330

    Method for calculate one square

--- razer.ino
+++ razer.ino
@@ -7,7 +7,6 @@
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-
 void createAddressableMatrix() {
   
   for ( int i = 1; i <= numVertical; i++ ) {
@@ -22,6 +21,44 @@ void createAddressableMatrix() {
     }
 
   }
+  
+}
+
+int *getSquare(int value) {
+
+  const int maxLedCountsInSquare = numVertical + numHorizontal + ( numVertical - 2 ) + ( numHorizontal - 2 ) + 1; // 61 mishe;
+
+  static int resultAddress[ (int) maxLedCountsInSquare ];
+
+  int ledCounts = maxLedCountsInSquare - ( value * 4 - 4 );
+
+  int k = 17;
+  int m = 32;
+  int i, j;
+  
+  for( int a = 1; a <= maxLedCountsInSquare; a++ ) {
+
+    if ( a <= 16 ) {
+      i = 1;
+      j = a;
+    } else if ( a > 16 && a <= 31 ) {
+      i = a - 15;
+      j = 16;
+    } else if ( a > 31 && a <= 46 ) {
+      i = 16;
+      j = a - k;
+      k = k + 2;
+    } else {
+      i = a - m;
+      j = 1;
+      m = m + 2;
+    }
+    
+    resultAddress[ a ] = ledFullMatrix[ i ][ j ];
+    
+  }
+
+  return resultAddress;
   
 }
 

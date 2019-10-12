a88c556ab7bf0d7bc8f9467f63d5542cffcd8856
Mohammadreza Yektamaram <mr.yekta@yahoo.com>
2019-10-12 11:08:48 +0330

    Added NeoPixel library

--- razer.ino
+++ razer.ino
@@ -1,7 +1,20 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_COUNT 256
+#define LED_PIN D1
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
+  
+  strip.begin();
+  strip.setBrightness(255);
+  strip.show();
   
 }
 
 void loop() {
 
+  strip.show();
+  
 }
